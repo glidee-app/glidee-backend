87aa1156231d2b30b9b854d42e4f480072937016
Quyum Kehinde <quyumkehinde@gmail.com>
2023-06-12 11:53:46 +0100

    chore: updated fields to work with frontend

--- app.py
+++ app.py
@@ -35,7 +35,6 @@ def index():
 
 # User Signup Route
 @app.post('/signup')
-
 @use_args({
     'first_name': fields.Str(required=True, error_messages={'required': 'The first_name field is required'}),
     'last_name': fields.Str(required=True, error_messages={'required': 'The last_name field is required'}),
@@ -43,7 +42,6 @@ def index():
     'password': fields.Str(required=True, error_messages={'required': 'The password field is required'}),
     'password_confirm': fields.Str(required=True, error_messages={'required': 'The password confirmation field is required'})
 }, location='json')
-
 def signup(data):
     if data['password'] != data['password_confirm']:
         return jsonify({'message': 'The passwords do not match.'}), 400
@@ -65,12 +63,10 @@ def signup(data):
 
 # User signin Route
 @app.post('/login')
-
 @use_args({
     'email': fields.Email(required=True),
     'password': fields.Str(required=True),
 }, location='json')
-
 def signin(data):
 
     user = (
@@ -83,7 +79,7 @@ def signin(data):
         return jsonify({'message': 'Invalid login credentials.'}), 400
 
     try:
-        auth_token = user.generate_auth_token(user.id)
+        auth_token = user.generate_auth_token()
     except Exception:
         return jsonify({'message': 'Error occured.'}), 500
 
@@ -93,15 +89,14 @@ def signin(data):
     }), 200
 
 # Forgot email route
-@app.route('/forgot_password', methods=['GET','POST'])
+@app.route('/forgot_password', methods=['GET', 'POST'])
 
 @use_args({
     'email': fields.Email(required=True, error_messages={'required': 'The email field is required'})
 }, location='json')
-
 def forgot_password(data):
 
-    email= data['email']
+    email = data['email']
 
     if not email:
         return jsonify({'message': 'Invalid login credentials.'}), 400
@@ -112,7 +107,6 @@ def forgot_password(data):
         return jsonify({'message': 'Invalid email. Please try again.'}), 400
 
     reset_token = token.send_token(email=email)
-    
 
     # Here you would send an email to the user containing the reset token
     # I plan to update this code later by using a service like SendGrid or Mailgun to handle this
@@ -121,14 +115,12 @@ def forgot_password(data):
 
 # Reset password route
 @app.route('/reset_password', methods=['GET', 'POST'])
-
 @use_args({
     'email': fields.Str(required=True, error_messages={'required': 'The first_name field is required'}),
     'token': fields.Str(required=True, error_messages={'required': 'The last_name field is required'}),
     'new_password': fields.Str(required=True, error_messages={'required': 'The password field is required'}),
     'confrim_password': fields.Str(required=True, error_messages={'required': 'The password confirmation field is required'})
 }, location='json')
-
 def reset_password(data):
 
     email = data['email']
@@ -158,16 +150,16 @@ def reset_password(data):
     if new_password != confirm_password:
         return jsonify({'message': 'The passwords you entered do not match. Please make sure that both passwords are the same.'}), 400
 
-
     user.set_password(new_password)
     db.session.commit()
 
     return jsonify({'message': 'Password successfully changed.'}), 200
+
 # fetch rides route
 @app.route('/fetch_rides', methods=['GET', 'POST'])
 @jwt_required()
 @use_args({
-    'comfortability': fields.Str(validate=validate.OneOf(['standard', 'premium']), required=True, error_messages={'required': 'The comfortability field is required'}),
+    'comfortability': fields.Str(validate=validate.OneOf(['Standard', 'Premium']), required=True, error_messages={'required': 'The comfortability field is required'}),
     'pickup_date': fields.Date(format='%Y-%m-%d', required=True, error_messages={'required': 'The pickup_datetime field is required'}),
     'pickup_location': fields.Str(required=True, error_messages={'required': 'The pickup_location field is required'}),
     'destination': fields.Str(required=True, error_messages={'required': 'The destination field is required'}),
@@ -177,6 +169,8 @@ def fetch_rides(data):
         Ride.query.join(Ride.vehicle)
         .filter(Vehicle.comfortability == data['comfortability'])
         .filter(Ride.pickup_date == data['pickup_date'])
+        .filter(Ride.pickup_location == data['pickup_location'])
+        .filter(Ride.destination == data['destination'])
         .filter(Ride.is_booked == False)
         .all()
     )
@@ -195,6 +189,7 @@ def fetch_rides(data):
             'driver': {
                 'id': ride_model.vehicle.driver_id,
                 'name': ride_model.vehicle.driver.name,
+                'phone_number': ride_model.vehicle.driver.phone_number,
             },
             'pickup_time': ride_model.pickup_time,
         })
@@ -205,13 +200,11 @@ def fetch_rides(data):
     }), 200
 
 
-@app.route('/create_order', methods = ["GET", "POST"])
+@app.route('/create_order', methods=["GET", "POST"])
 @jwt_required()
-
 @use_args({
     'ride_id': fields.Int(required=True, validate=validate.Range(min=1), error_messages={'required': 'The ride_id field is required'}),
 }, location='json')
-
 def create_order(data):
 
     user = get_jwt_identity()
@@ -227,25 +220,22 @@ def create_order(data):
     order = Order(
         user_id=user['user_id'],
         ride_id=data['ride_id'],
-        status = 'upcoming_trip'
+        status='upcoming_trip'
     )
 
     db.session.add(order)
     db.session.commit()
     return jsonify({'message': 'Order created successfully'}), 201
 
+
 @app.post('/cancel_order')
 @jwt_required()
 @use_args({
     'order_id': fields.Int(validate=validate.Range(min=1), required=True, error_messages={'required': 'The order_id field is required'}),
-    'ride_id': fields.Int(required=True, validate=validate.Range(min=1), error_messages={'required': 'The vehicle_id field is required'})
-    }, location='json')
-
-
+}, location='json')
 def cancel_order(data):
     user = get_jwt_identity()
-    
-    ride_id = data['ride_id']
+
     order = Order.query.filter_by(
         id=data['order_id'],
         user_id=user['user_id']
@@ -254,33 +244,30 @@ def cancel_order(data):
     if not order:
         return jsonify({'message': 'Invalid order ID'}), 400
 
-    ride = Ride.query.get(ride_id)
-    if ride: 
-        ride.is_booked = False # make ride available again so it can be booked
+    ride = Ride.query.get(order.ride_id)
+    if ride:
+        ride.is_booked = False  # make ride available again so it can be booked
 
-    order.status = 'cancelled_order'  
+    order.status = 'cancelled_trip'
     db.session.commit()
 
     return jsonify({'message': 'Order cancelled successfully'}), 200
 
 
-@app.route('/order_history', methods =["GET", "POST"])
+@app.route('/order_history', methods=["GET", "POST"])
 @jwt_required()
-
 @use_args({
     'status': fields.Str(validate=validate.OneOf(['upcoming_trip', 'completed_trip', 'cancelled_trip']), required=True, error_messages={'required': 'The status field is required'})
 }, location='query')
-
 def get_user_orders(data):
-    order_status= data['status']
+    order_status = data['status']
 
     user = get_jwt_identity()
-
 
     order_models = (
         Order.query
         .filter_by(user_id=user['user_id'])
-        .filter_by(status = order_status)
+        .filter_by(status=order_status)
         .order_by(Order.created_at)
         .all()
     )
@@ -293,14 +280,19 @@ def get_user_orders(data):
                 'pickup_location': order_model.ride.pickup_location,
                 'destination': order_model.ride.destination,
                 'pickup_time': order_model.ride.pickup_time,
-                'amount': order_model.ride.amount,
+                'amount': order_model.ride.vehicle.amount,
                 'pickup_date': order_model.ride.pickup_date,
                 'vehicle': {
                     'id': order_model.ride.vehicle.id,
                     'make': order_model.ride.vehicle.make,
-                    'model': order_model.ride.vehicle.make,
+                    'model': order_model.ride.vehicle.model,
                     'license_plate': order_model.ride.vehicle.license_plate,
                     'comfortability': order_model.ride.vehicle.comfortability,
+                    'driver': {
+                        'id': order_model.ride.vehicle.driver.id,
+                        'name': order_model.ride.vehicle.driver.name,
+                        'phone_number': order_model.ride.vehicle.driver.phone_number,
+                    }
                 },
                 'status': order_model.status,
             })
@@ -312,6 +304,7 @@ def get_user_orders(data):
         'message': 'Orders fetched successfully'
     }), 200
 
+
 @app.route('/seed_drivers', methods=["GET", "POST"])
 def seed_drivers():
 
@@ -320,7 +313,8 @@ def seed_drivers():
             data = json.load(file)
             drivers = data[0]['details']
             for driver in drivers:
-                db.session.add(Driver(name=driver['name'], phone_number=driver['phone_number'], email=driver['email']))
+                db.session.add(Driver(
+                    name=driver['name'], phone_number=driver['phone_number'], email=driver['email']))
             db.session.commit()
 
     except Exception as e:
@@ -328,7 +322,6 @@ def seed_drivers():
         return jsonify({'message': f'Error occurred while seeding drivers. {e}'}), 500
 
     return jsonify({'message': 'Drivers seeded successfully.'}), 200
-
 
 
 # Route to seed the data into the database
@@ -355,6 +348,7 @@ def seed_vehicles():
 
     return jsonify({'message': 'Vehicles seeded successfully.'}), 200
 
+
 @app.route('/seed_rides', methods=["GET", "POST"])
 def seed_rides():
 
@@ -368,7 +362,7 @@ def seed_rides():
 
             vehicle_id = ride_data['vehicle_id']
             pickup_location = ride_data['pickup_location']
-            destination = ride_data['destination']            
+            destination = ride_data['destination']
             pickup_date = date(year, month, day)
             pickup_time = ride_data['pickup_time']
             is_booked = ride_data['is_booked']
@@ -403,10 +397,9 @@ def my_expired_token_callback(jwt_header, jwt_value=None):
     }), 401
 
 
-
 @app.errorhandler(422)
 @app.errorhandler(400)
-def handle_error(err): 
+def handle_error(err):
     messages = []
     errors = err.data.get('messages', {}).get('json', {})
     for key in errors.keys():
--- models.py
+++ models.py
@@ -68,10 +68,9 @@ class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     ride_id = db.Column(db.Integer, db.ForeignKey('rides.id'), nullable=False)
-    # 1 represents active while 0 represents cancelled
     ride = db.relationship(
         'Ride', back_populates='orders')
-    status = db.Column(db.String(50), nullable=False, default= None)
+    status = db.Column(db.String(50), nullable=False, default=None)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
@@ -87,7 +86,7 @@ class Ride(db.Model):
     destination = db.Column(db.String(50), nullable=False)
     pickup_date = db.Column(db.Date, nullable=False)
     pickup_time = db.Column(db.String(50), nullable=False)
-    is_booked = db.Column(db.Boolean, nullable = False, default = False)
+    is_booked = db.Column(db.Boolean, nullable=False, default=False)
     available_seats = db.Column(db.Integer, nullable=False)
     orders = db.relationship(
         'Order', back_populates='ride', foreign_keys="Order.ride_id", lazy=True)
