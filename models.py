83df910a5813fa1a3945c8cad742c0a8f068f3d2
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2023-06-13 16:12:09 +0100

    fix: fix login

--- models.py
+++ models.py
@@ -13,7 +13,6 @@ class User(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    admin = db.Column(db.Boolean, nullable=False, default=False)
     password_hash = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(
